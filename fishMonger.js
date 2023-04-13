30aba1a8dd30b7e2108f33129d3dcf9a0a56c772
William Briscoe <will.m.briscoe@gmail.com>
2023-04-13 15:11:27 -0500

    secondary comments

--- fishMonger.js
+++ fishMonger.js
@@ -1,16 +1,20 @@
-/ import boatInventory function
+// import boatInventory function
 //invoke boatInventory, which generates todaysCatch array
+
 //create a function called fishmonger with a for of loop that iterates through todaysCatch
     //declare empty array called mongerInventory []
     //create ifand statement that checks if fish price is <= 7.50  && fish quantity >= 10
     //then .push array method to add objects that meet the above requirements to the empty mongerInventory array in increments of 10
     //modify the amount property of each object to = 10
     //return mongerInventory array
-//create a function called chefsOrder with a for of loop that iterates through mongerInventory array
+
+    //create a function called chefsOrder with a for of loop that iterates through mongerInventory array
     //parameters are chefsBudget (a number) and an array (mongerInventory)
     //declare empty array called chefsInventory []
     //create if statement that checks if fish price is <= chefBudget
     //then divide the amount property of each object by 2. (mongerInventory.amount = math.floor(mongerInventory.amount / 2 ) )
     //use .push array method to add objects that meet the above requirements to the empty chefsInventory array
     //return chefsInventory array
-//export fishmonger and chefsOrder functions to restaurant.js
+
+
+    //export fishmonger and chefsOrder functions to restaurant.js
